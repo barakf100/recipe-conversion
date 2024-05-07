ebad48d5c5b477231a417cf3aac24719561a80b4
barak froimovich <barakf100@gmail.com>
2024-05-07 18:00:45 +0300

    ads

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
+import * as Mui from "@mui/material";
 import ButtonComp from "./components/button";
+import Ads from "./components/ads";
 function App() {
     return (
-        <>
-            <ButtonComp text="Hello World" />
-        </>
+        <Mui.Container maxWidth="false">
+            <Mui.Box width="100%">
+                {/* nav bar */}
+                <ButtonComp text={"Home"} />
+            </Mui.Box>
+            <Mui.Grid container>
+                <Mui.Grid item xs={12} md={8}>
+                    {/* main */}
+                </Mui.Grid>
+                <Mui.Grid item xs={12} md={4}>
+                    <Ads />
+                </Mui.Grid>
+            </Mui.Grid>
+            <Mui.Box width="100%">{/* footer */}</Mui.Box>
+        </Mui.Container>
     );
 }
 
---
+++ src/components/ads.jsx
@@ -0,0 +1,10 @@
+const Ads = () => {
+    return (
+        <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8888560223020815"
+            crossOrigin="anonymous"></script>
+    );
+};
+
+export default Ads;
